bff488a819396d11ff8305e1c2812932da9d5e33
EnvOut <31275761+EnvOut@users.noreply.github.com>
2024-01-15 22:57:14 +0200

    the number of types that implement the message has been increased
    - i numbers
    - u numbers
    - String
    - NonZero I
    - NonZero U
    - option
    - arc
    - mutex
    - rwlock

--- src/uactor/src/lib.rs
+++ src/uactor/src/lib.rs
@@ -1,4 +1,6 @@
-use tokio::sync::{broadcast, mpsc, oneshot, watch};
+use std::num::{NonZeroI16, NonZeroI32, NonZeroI64, NonZeroI8, NonZeroU16, NonZeroU32, NonZeroU64, NonZeroU8};
+use std::sync::Arc;
+use tokio::sync::{broadcast, mpsc, Mutex, oneshot, RwLock, watch};
 use tokio::task::JoinHandle;
 use crate::select::{ActorSelect};
 
@@ -272,7 +274,35 @@ impl<T> DataSource for oneshot::Receiver<T> where T: Send {
 pub trait Message { }
 
 impl<A, B> Message for Result<A, B> where A: Message, B: Message {}
+impl<A> Message for Option<A> where A: Message {}
+impl<A> Message for Arc<A> where A: Message {}
+impl<A> Message for Mutex<A> where A: Message {}
+impl<A> Message for RwLock<A> where A: Message {}
 
+impl Message for i64 {}
+impl Message for i32 {}
+impl Message for i16 {}
+impl Message for i8 {}
+
+impl Message for u64 {}
+impl Message for u32 {}
+impl Message for u16 {}
+impl Message for u8 {}
+
+impl Message for f64 {}
+impl Message for f32 {}
+
+impl Message for String {}
+
+impl Message for NonZeroI64 {}
+impl Message for NonZeroI32 {}
+impl Message for NonZeroI16 {}
+impl Message for NonZeroI8 {}
+
+impl Message for NonZeroU64 {}
+impl Message for NonZeroU32 {}
+impl Message for NonZeroU16 {}
+impl Message for NonZeroU8 {}
 
 #[async_trait::async_trait]
 pub trait Handler<M>
